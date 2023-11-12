61e3cca38ca6eb7e384e4c7df3c479ac1b266290
moroznastya <nastya.moroz2210@gmail.com>
2023-11-13 01:36:10 +0200

    edit

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-//-----------------------------------------------------------------------------
-// NodeJS App for GCP Cloud Functions deployed via GCP Cloud Build Triggers
-// Copyleft (c) by Denis Astahov
-//-----------------------------------------------------------------------------
+
 
 exports.helloWorld = (req, res) => {
   const message="<font color='blue'>СloudFunction</font><br><b>App Version 1.1</b>";
